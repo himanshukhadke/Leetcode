a277ab4840b849aa07558377a2b21c90ba7b82a3
Himanshu Khadke <khadkeh754@gmail.com>
2023-09-02 12:31:55 +0530

    Time: 201 ms (47.85%) | Memory: 74.7 MB (63.40%) - LeetSync

--- 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
@@ -7,9 +7,7 @@ public:
             return dp[i];
         int ans = s.length()-i;
         for(int j=i;j<s.length();++j){
-            // cout<<i<<" "<<j<<endl;
             if(find(dictionary.begin(),dictionary.end(),s.substr(i,j-i+1))!=dictionary.end()){
-                //cout<<s.substr(i,j-i+1)<<endl;
                 ans = min( ans, func(s,j+1,dictionary));
             }
             ans = min (ans, j-i+1 + func(s,j+1,dictionary));
@@ -18,7 +16,18 @@ public:
     }
 
     int minExtraChar(string s, vector<string>& dictionary) {
-        memset(dp,-1,sizeof(dp));
-        return func(s, 0, dictionary);
+        memset(dp,0,sizeof(dp));
+        int n = s.length();
+        for(int i=n-1;i>=0;i--){
+            int ans = n-i;
+            for(int j=i;j<n;++j){
+                if(find(dictionary.begin(),dictionary.end(),s.substr(i,j-i+1))!=dictionary.end()){
+                    ans = min( ans, dp[j+1]);
+                }
+                ans = min (ans, j-i+1 + dp[j+1]);
+            }
+            dp[i] = ans;
+        }
+        return dp[0];
     }
 };
